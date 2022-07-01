6d5c04d1143f12fa9c5e56875937337258cfa669
Fahad Murtaza <fahdim@gmail.com>
2022-07-02 01:34:40 +0500

    Add a new record if the power status changes

--- index.js
+++ index.js
@@ -3,31 +3,45 @@ const chalk = require("chalk");
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
 
-(async () => {
-  const { level, charging } = await battery();
+setInterval(() => {
+  (async () => {
+    const { level, charging } = await battery();
 
-  if (level > 0.5) {
-    levelColour = "greenBright"
-  } else if (level > 0.2) {
-    levelColour = "yellowBright"
-  }
+    if (level > 0.5) {
+      levelColour = "greenBright"
+    } else if (level > 0.2) {
+      levelColour = "yellowBright"
+    }
 
-  console.log(`${charging ? chalk.greenBright("Charging") : chalk.redBright("Not charging")}, ${chalk[levelColour](`${Math.round(level * 100)}%`)}`)
+    console.log(`${charging ? chalk.greenBright("Charging") : chalk.redBright("Not charging")}, ${chalk[levelColour](`${Math.round(level * 100)}%`)}`)
 
+    MongoClient.connect(url, function (err, db) {
 
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    const dbo = db.db("bujhlee");
-    let bijliStatus = { level: level, charging: charging };
-    dbo.collection("bijlistatus").insertOne(bijliStatus, function (err, res) {
       if (err) throw err;
-      console.log("1 Bujhlee ...");
-      db.close();
+      const dbo = db.db("bujhlee");
+
+      dbo.collection('bijlistatus').findOne(
+        {},
+        { sort: { _id: -1 } },
+        (err, data) => {
+          console.log(data.charging);
+
+          if (err) throw err;
+
+          if (data.charging !== charging) {
+            let bijliStatus = { level: level, charging: charging, time: new Date() };
+            console.log(charging);
+            dbo.collection("bijlistatus").insertOne(bijliStatus, function (err, res) {
+              if (err) throw err;
+              console.log("Record Entered ...");
+              db.close();
+            });
+          }
+        },
+      );
+
     });
-  });
 
-  setTimeout(() => {
+  })();
 
-  });
-
-})();
+}, 300000);
